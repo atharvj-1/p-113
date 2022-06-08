d315970a04dc1067fbb8e674b3b8a1f1b3eaf551
atharvj-1 <89965210+atharvj-1@users.noreply.github.com>
2022-06-08 16:52:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,10 @@ function preload(){
 function setup(){
     canvas=createCanvas(800,400);
     canvas.position(100,250);
-    circle(40, 40, 60);
-    circle(40, 350, 60);
-    circle(750, 350, 60);
-    circle(750, 40, 60);
-    rect(35, 70, 10, 250);
-    rect(745, 70, 10, 250);  
-    rect(70, 350,650, 10);
-    rect(70, 35,650, 10);
+    video=createCapture(VIDEO);
+    video.position(100,250);
+    video.hide();
+    
     
     
 
@@ -20,8 +16,21 @@ function setup(){
 }
 
 function draw(){
-    image(video,0,0,700,500);
+    image(video,100,50,600,300);
     tint(tint_color);
+    fill(255,0,0);
+    stroke(255,0,0);
+        
+    circle(40, 40, 60);
+    circle(40, 350, 60);
+    circle(750, 350, 60);
+    circle(750, 40, 60);
+    stroke('green');
+    fill('green')
+    rect(35, 70, 10, 250);
+    rect(745, 70, 10, 250);  
+    rect(70, 350,650, 10);
+    rect(70, 35,650, 10);
 }
 
 function filter_tint(){
