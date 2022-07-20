438baf43fb31b2a9ecf837221c6bfef44bd3941f
atharvj-1 <89965210+atharvj-1@users.noreply.github.com>
2022-07-20 20:50:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+noseX=0;
+noseY=0;
+
+
 function preload(){
+    mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+    lipstick=loadImage("https://i.postimg.cc/PxFvYgkv/l1.png")
 
 }
 
@@ -9,14 +15,32 @@ function setup(){
     video.position(100,250);
     video.hide();
     
+    poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("posenet_is_loaded");
+}
+
+function gotPoses(results){
+if(results.length > 0 ){
+    console.log(results);
+    console.log("nosex="+ results[0].pose.nose.x);
+    console.log("nosey="+ results[0].pose.nose.y);
+    mustacheX= results[0].pose.nose.x15;
+   mustacheY=results[0].pose.nose.y10;
+}
+}
     
     
 
     tint_color="";
-}
+
 
 function draw(){
     image(video,100,50,600,300);
+    image(mustache,mustacheX,mustacheY,30,30);
+
     tint(tint_color);
     fill(255,0,0);
     stroke(255,0,0);
